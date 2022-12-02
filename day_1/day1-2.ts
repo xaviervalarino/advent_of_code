8285211e0c39be6111100f27f3ecd6256ef0e092
Xavier Valarino <xvalarino@gmail.com>
2022-12-01 23:38:54 -0800

    Complete second solution for Day 1

---
+++ day_1/day1-2.ts
@@ -0,0 +1,21 @@
+import transformer from "../modules/transformer.ts";
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const inputFile = __dirname + "input.txt"
+
+const totalCaloriesPerElf: number[] = [];
+
+transformer(inputFile, async (readlines) => {
+  let index = 0;
+  for await (const line of readlines) {
+    if (line.length) {
+      const prev = totalCaloriesPerElf[index] || 0;
+      const current = +line;
+      totalCaloriesPerElf[index] = prev + current;
+    } else {
+      index++;
+    }
+  }
+  const topThreeCalorieCounts = totalCaloriesPerElf.sort((a, b) => a - b).slice(totalCaloriesPerElf.length - 3);
+  return topThreeCalorieCounts.reduce((a,b) => a + b).toString()
+});
