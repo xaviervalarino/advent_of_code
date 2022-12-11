f0848ddd64ab2953676e16189d4ad8224cb61716
Xavier Valarino <xvalarino@gmail.com>
2022-12-11 15:04:54 -0800

    Sovle day 7, 1st half

---
+++ solutions/07.1.ts
@@ -0,0 +1,47 @@
+/*
+ * Advent of Code
+ * Day 7 - 1st Solution
+ * * * * * * * * * * * * * * *
+ * w|!deno run --allow-read %
+ */
+
+import transformer from "../modules/transformer.ts";
+
+type DiskUsage = {
+  [name: string]: number;
+};
+
+transformer("./inputs/07.txt", async (readlines) => {
+  let dirSizes = 0;
+  const du: DiskUsage = {};
+  const currentDirs: string[] = [];
+  for await (const line of readlines) {
+    const dirChange = /^\$ cd (.+)/.exec(line);
+    const filesize = /^\d+/.exec(line);
+
+    if (dirChange) {
+      const arg = dirChange[1];
+      if (arg == "..") {
+        currentDirs.pop();
+      } else {
+        const path =
+          arg === "/" ? arg : currentDirs[currentDirs.length - 1] + "/" + arg;
+        path.replace("//", "/");
+        currentDirs.push(path);
+        if (!du[path]) du[path] = 0;
+      }
+    }
+    if (filesize) {
+      for (const dir of currentDirs) {
+        du[dir] += +filesize[0];
+      }
+    }
+  }
+  for (const dir in du) {
+    const filesize = du[dir];
+    if (filesize <= 100000) {
+      dirSizes += du[dir];
+    }
+  }
+  return dirSizes.toString();
+});
