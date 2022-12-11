c89c0511029e76f4a89bbdfacde8217ca3936056
Xavier Valarino <xvalarino@gmail.com>
2022-12-11 15:15:51 -0800

    Solve day 7, 2nd half

---
+++ solutions/07.2.ts
@@ -0,0 +1,50 @@
+/*
+ * Advent of Code
+ * Day 7 - 2nd Solution
+ * * * * * * * * * * * * * * *
+ * w|!deno run --allow-read %
+ */
+
+import transformer from "../modules/transformer.ts";
+
+type DiskUsage = {
+  [name: string]: number;
+};
+
+transformer("./inputs/07.txt", async (readlines) => {
+  const du: DiskUsage = {};
+  const currentDirs: string[] = [];
+  for await (const line of readlines) {
+    const dirChange = /^\$ cd (.+)/.exec(line);
+    const filesize = /^\d+/.exec(line);
+
+    if (dirChange) {
+      const arg = dirChange[1];
+      if (arg == "..") {
+        currentDirs.pop();
+      } else {
+        const path =
+          arg === "/" ? arg : currentDirs[currentDirs.length - 1] + "/" + arg;
+        path.replace("//", "/");
+        currentDirs.push(path);
+        if (!du[path]) du[path] = 0;
+      }
+    }
+    if (filesize) {
+      for (const dir of currentDirs) {
+        du[dir] += +filesize[0];
+      }
+    }
+  }
+  // return dirSizes.toString();
+  const spaceNeeded = 30000000 - (70000000 - du["/"]);
+  const candidates = [];
+  for (const dir in du) {
+    const filesize = du[dir];
+    if (filesize >= spaceNeeded) {
+      candidates.push(filesize);
+    }
+  }
+  candidates.sort((a, b) => a - b);
+  return candidates[0].toString();
+});
