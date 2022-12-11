3e69b944e09ea008e22317b9a9bec4abae9b934b
Xavier Valarino <xvalarino@gmail.com>
2022-12-11 12:11:43 -0800

    Solve day 6, 1st half

---
+++ solutions/06.1.ts
@@ -0,0 +1,26 @@
+/*
+ * Advent of Code
+ * Day 6 - 1st Solution
+ * * * * * * * * * * * * * * *
+ * w|!deno run --allow-read %
+ */
+
+import transformer from "../modules/transformer.ts";
+
+transformer("./inputs/06.txt", async (readlines) => {
+  let startOfMarker = "";
+  for await (const datastream of readlines) {
+    for (let i = 0; i < datastream.length; i++) {
+      const marker = datastream.substring(i, i + 4);
+      const test = [...marker].every((char, _, arr) => {
+        const indices = arr.flatMap((l, i) => (char === l ? i : []));
+        return indices.length === 1;
+      });
+      if (test) {
+        startOfMarker = (i + 4).toString();
+        break;
+      }
+    }
+  }
+  return startOfMarker;
+});
